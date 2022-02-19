0058d3ba60c281bae9800bfb2d47467b7bad5eb1
Jamie Taffurelli <jltaffurelli@outlook.com>
2022-02-19 16:03:01 +0000

    added module

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# az-nsg
+# Azure Network Security Group module
+
+This repo contains a [Terraform](https://www.terraform.io/) module that defines an Azure Network Security Group
---
+++ az-nsg.tf
@@ -0,0 +1,90 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.95"
+    }
+  }
+
+  required_version = "~> 1.1.5"
+}
+
+data "azurerm_log_analytics_workspace" "logs" {
+  name                = var.log_analytics_workspace_name
+  resource_group_name = var.log_analytics_workspace_resource_group_name
+}
+
+data "azurerm_storage_account" "logs" {
+  name                = var.storage_account_name
+  resource_group_name = var.storage_account_resource_group_name
+}
+
+resource "azurerm_network_security_group" "network" {
+  name                = var.network_security_group_name
+  resource_group_name = azurerm_resource_group.network.name
+  location            = var.location
+  tags                = var.tags
+}
+
+resource "azurerm_monitor_diagnostic_setting" "network_security_group_diagnostics" {
+  name                       = "security-logging"
+  target_resource_id         = azurerm_network_security_group.network.id
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.logs.id
+
+  log {
+    category = "NetworkSecurityGroupEvent"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = 365
+    }
+  }
+
+  log {
+    category = "NetworkSecurityGroupRuleCounter"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = 365
+    }
+  }
+}
+
+resource "azurerm_network_watcher_flow_log" "network" {
+  network_watcher_name      = var.network_watcher_name
+  resource_group_name       = var.network_watcher_resource_group_name
+  network_security_group_id = azurerm_network_security_group.network.id
+  storage_account_id        = data.azurerm_storage_account.logs.id
+  enabled                   = true
+  version                   = 2
+
+  retention_policy {
+    enabled = true
+    days    = 365
+  }
+
+  traffic_analytics {
+    enabled               = true
+    workspace_id          = data.azurerm_log_analytics_workspace.logs.workspace_id
+    workspace_region      = data.azurerm_log_analytics_workspace.logs.location
+    workspace_resource_id = data.azurerm_log_analytics_workspace.logs.id
+    interval_in_minutes   = 10
+  }
+  tags = var.tags
+}
+
+resource "azurerm_network_security_rule" "network" {
+  name                        = "in-rdp"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.network.name
+  network_security_group_name = azurerm_network_security_group.network.name
+}
